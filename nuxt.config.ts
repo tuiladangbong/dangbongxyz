a38408802a37afa1295a30d066239dc6b96bc907
tuiladangbong <tuiladangbong@outlook.com>
2025-11-02 18:47:52 +0700

    Update all changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,5 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default {
-  // Server configuration
-  server: {
-    host: '0.0.0.0', // listen on all IPs
-    port: 3000
-  },
-  // Global CSS
-  css: ['~/assets/css/main.css'],
-  // Auto import components
-  components: true,
-  // Modules (Pinia for state management)
-  modules: ['@pinia/nuxt']
-}
+// nuxt.config.ts
+export default defineNuxtConfig({
+  server: { host: '0.0.0.0', port: 3000 },
+  ssr: true
+})
