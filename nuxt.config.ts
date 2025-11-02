bb536831905533032327bd7a733fe490d5360496
tuiladangbong <tuiladangbong@outlook.com>
2025-11-02 08:20:13 +0700

    Update Nuxt configuration for server settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+export default {
+  server: {
+    host: '0.0.0.0',
+    port: 3000
+  }
+}
